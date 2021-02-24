ef5cbb4a9989aa654fb9d1dd3a5676dd3762738d
Aziz Bohra <aziz@creativity.com.kw>
2021-02-24 12:40:09 +0300

    add jquery ajaxsetup for csrf-token

--- public/js/app.js
+++ public/js/app.js
@@ -37353,8 +37353,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/macbookpro/Sites/samples/laravel/laravel-template/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/macbookpro/Sites/samples/laravel/laravel-template/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/macbookpro/Sites/samples/laravel-template-with-adminlte/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/macbookpro/Sites/samples/laravel-template-with-adminlte/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,23 @@
 <!-- jQuery -->
-<script src="{{ asset('vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('../vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('vendor/almasaeed2010/adminlte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('../vendor/almasaeed2010/adminlte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('../vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('vendor/almasaeed2010/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<script src="{{ asset('../vendor/almasaeed2010/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('../vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js') }}"></script>
+
+<!-- CSRF Protection -->
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
