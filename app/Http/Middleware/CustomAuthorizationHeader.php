f865489d9323c309f3dd5a56b9f2564d31d2906d
Aziz Bohra <aziz@creativity.com.kw>
2021-08-04 09:59:01 +0300

    add cors support

--- app/Http/Middleware/CustomAuthorizationHeader.php
+++ app/Http/Middleware/CustomAuthorizationHeader.php
@@ -9,14 +9,14 @@ class CustomAuthorizationHeader
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if($request->headers->has('X-Access-Token')){
-            $request->headers->set('Authorization', $request->headers->get('X-Access-Token'));
+        if ($request->headers->has('X-Auth-Token')) {
+            $request->headers->set('Authorization', $request->headers->get('X-Auth-Token'));
         }
 
         return $next($request);
